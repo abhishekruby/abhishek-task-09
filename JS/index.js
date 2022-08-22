9b39a658df1d1972463dcbf93cfe05421b043181
abhishekruby <abhishekruby94@gmail.com>
2022-08-22 23:14:09 +0530

    contact section completed

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+
+window.addEventListener("load", ()=>{
+    document.querySelectorAll(".text-input").forEach((element) => {
+        element.addEventListener("blur", (event) => {
+          if (event.target.value != "") {
+            event.target.nextElementSibling.classList.add("filled");
+          } else {
+            event.target.nextElementSibling.classList.remove("filled");
+          }
+        });
+      });
+})
